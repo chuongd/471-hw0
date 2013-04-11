15b6025b9cfee44efc9cbaedaf8443f8499333d5
Kyle Boone <boonekr@cs.washington.edu>
2013-04-11 13:20:50 -0700

    implemented global history predictor without branch history tables have not yet ran benchmarks

--- HW1-BranchPrediction/TwoLevelAdaptivePredictor.cpp
+++ HW1-BranchPrediction/TwoLevelAdaptivePredictor.cpp
@@ -1,17 +1,46 @@
 #include "TwoLevelAdaptivePredictor.h"
+#include <cassert>
 
-TwoLevelAdaptivePredictor::TwoLevelAdaptivePredictor() : Predictor(){
-
+TwoLevelAdaptivePredictor::TwoLevelAdaptivePredictor()
+    : Predictor(), patternHistoryTable(1 << HISTORY_LENGTH) {
+  mask = (1 << HISTORY_LENGTH) - 1;
 }
 
-BranchOutcome TwoLevelAdaptivePredictor::predictBranch(ADDRINT addr){
- 
-    return Taken;
-  
+BranchOutcome TwoLevelAdaptivePredictor::predictBranch(ADDRINT addr) {
+
+  uint16_t index = addr & mask;
+  assert(index < patternHistoryTable.size());
+  int state = patternHistoryTable[index];
+  switch (state) {
+    case TAKEN:
+    case WEAK_TAKEN:
+      return Taken;
+    case NOT_TAKEN:
+    case WEAK_NOT_TAKEN:
+      return NotTaken;
+    default:
+      // shouldn't get to this state
+      assert(false);
+      return Taken;
+  }
+
 }
 
 void TwoLevelAdaptivePredictor::updatePredictor(ADDRINT addr, BranchOutcome o){
 
+  // update globalBranchHistory
+  globalBranchHistory << 1;
+  if (o == Taken)
+    globalBranchHistory | 0x01;
+
+  // update state
+  uint16_t index = globalBranchHistory & mask;
+  assert(index < patternHistoryTable.size());
+  int state = patternHistoryTable[index];
+  if (o == Taken && state != TAKEN)
+    patternHistoryTable[index]++;
+  else if (o == NotTaken && state != NOT_TAKEN)
+    patternHistoryTable[index]--;
 
 }
 
--- HW1-BranchPrediction/TwoLevelAdaptivePredictor.h
+++ HW1-BranchPrediction/TwoLevelAdaptivePredictor.h
@@ -1,6 +1,23 @@
 #include "Predictor.h"
+#include <vector>
+
+// 4KB budget - 32768 bits
+// 32768 / 2 bits per buffer = 16384 entries max
+// log 16384 = 14
+// however, history will use some of the budget, so will use 1 less
+// this leaves a total of 8192 entries
+#define HISTORY_LENGTH 13
+
+#define NOT_TAKEN 0
+#define WEAK_NOT_TAKEN 1
+#define WEAK_TAKEN 2
+#define TAKEN 3
 
 class TwoLevelAdaptivePredictor: public Predictor{
+private:
+  uint16_t globalBranchHistory;
+  uint16_t mask;
+  std::vector<int> patternHistoryTable;
 
 public:
   TwoLevelAdaptivePredictor();  
